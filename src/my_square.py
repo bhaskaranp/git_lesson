09e053cd73c204d0668b5af30e2abfdb03a97448
bhaskaran Periasamy <bhaskaran.periasamy@gmail.com>
2018-05-11 09:48:59 +0100

    added function

---
+++ src/my_square.py
@@ -0,0 +1,12 @@
+def my_square(x):
+	"""taks a value and returns squared value.
+	uses opearator **
+	test
+	"""
+	return (x ** 2)
+
+def add(a,b):
+	return a+b
+
+print(my_square(5))
+print(add(5,6))
